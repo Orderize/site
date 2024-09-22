08ccc98b83917e6bf25f092ea9829484c02b13aa
Thiago Santos <134028982+Sant-Thiago@users.noreply.github.com>
2024-09-22 16:32:09 +0000

    [FEAT] uploading paths

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,9 @@ import { Route, Routes, BrowserRouter } from 'react-router-dom'
 import { createRoot } from 'react-dom/client'
 import Login from './pages/login/login'
 import Flavor from './pages/options/flavor'
+import ModalDrink from './components/modal/drink/drink';
+import ModalFlavor from './components/modal/flavor/flavor';
+import ModalReview from './components/modal/review/review';
 import Navbar from './components/navbar/navbar'
 import Order from './pages/new_order/order'
 import './index.css'
@@ -32,7 +35,20 @@ createRoot(document.getElementById('root')).render(
           element={ <Flavor /> }
         ></Route>
 
-        
+        <Route
+          path='/modal/sabores'
+          element={ <ModalFlavor /> }
+        ></Route>
+
+        <Route
+          path='/modal/bebidas'
+          element={ <ModalDrink /> }
+        ></Route>
+
+        <Route
+          path='/modal/revisoes'
+          element={ <ModalReview /> }
+        ></Route>
 
       </Routes>
     </BrowserRouter>
